1060d7f3f7f646d4462ea659977c0cde34143169
aaaimee000 <86871018+aaaimee000@users.noreply.github.com>
2025-10-31 18:01:18 +0000

    chore(frontend & backend): ignore build artifacts and dependencies

--- backend/LLM/client.py
+++ backend/LLM/client.py
@@ -3,7 +3,10 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 
-load_dotenv()
+# Make it find the .env file in backend folder
+env_path = os.path.join(os.path.dirname(__file__), "..", ".env")
+load_dotenv(dotenv_path=env_path)
+
 
 #Create a single OpenAI client using the API key from environment
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
