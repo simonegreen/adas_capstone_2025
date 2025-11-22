933ede5d775b9c2eff19f1e8d8f95f9bcced16d2
abby-nelson <abby5655@gmail.com>
2025-11-22 22:08:11 +0000

    just merging

--- run_test.py
+++ run_test.py
@@ -3,7 +3,7 @@ import os, sys
 sys.path.insert(0, os.path.join(os.getcwd(), "backend"))
 from backend.backendInterface import add_data, find_anomalies, get_output
 
-data_path = "/root/adas_capstone_2025/data/real-world/data-cleaning/half.csv"
+data_path = "/root/adas_capstone_2025/data/real-world/data-cleaning/quadrupled.csv"
 structure = {"top_n":None, "num_features":None, "start":None, "end": None, "target_ip":None, "explanation": None, "sort_by": None, "uid_column": None}
 add_data(data_path)
 anoms = find_anomalies(query=structure, uid="uid", num_feat=10, time=None, source_ip="uid")
